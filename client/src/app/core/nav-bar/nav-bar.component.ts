1b8c06469d40a35a0bf982277ab74fa7025937b5
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2024-04-08 00:52:46 +0700

    client: update login form and navbar

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { BasketService } from 'src/app/basket/basket.service';
 import { BasketItem } from 'src/app/shared/models/basket';
+import { AccountService } from '../../account/account.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'] 
 })
 export class NavBarComponent {
 
-  constructor(public basketService: BasketService) {}
+  constructor(public basketService: BasketService, public accountService: AccountService) {}
 
   getCount(items: BasketItem[]) {
     return items.reduce((sum, item) => sum + item.quantity, 0);
